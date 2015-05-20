987583598c9cf1aa6b59082e9f332f2c6b533439
jimmay5469 <jimmay5469@gmail.com>
2015-05-19 23:53:45 -0400

    Step4: Move record transform to normalize

--- app/user/serializer.js
+++ app/user/serializer.js
@@ -2,13 +2,15 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   extractSingle: function(store, primaryTypeClasss, payload, recordId) {
-    payload = {
-      user: {
-        id: payload.login,
-        login: payload.login,
-        avatarUrl: payload.avatar_url
-      }
-    };
+    payload = { user: payload };
     return this._super(store, primaryTypeClasss, payload, recordId);
+  },
+  normalize: function(typeClass, hash, prop) {
+    hash = {
+      id: hash.login,
+      login: hash.login,
+      avatarUrl: hash.avatar_url
+    };
+    return this._super(typeClass, hash, prop);
   }
 });
